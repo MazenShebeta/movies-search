f3bf4be3eaeb5e8ee2b117af50c5095d942f558b
MazenShebeta <64854496+MazenShebeta@users.noreply.github.com>
2022-10-12 18:19:53 +0200

    fixed routing problem

--- src/Components/MoviePage.js
+++ src/Components/MoviePage.js
@@ -8,20 +8,26 @@ import { Link } from 'react-router-dom';
 
 function MoviePage() {
     const { id } = useParams();
-    const movie = data.find((movie) => movie.id === id);
+    const movie = data.find((movie) => movie.id == id);
     console.log(movie);
     return (
-        <div className='movie-page'>
-            <div className='movie-page-content'>
-                <div className='movie-page-image'>
-                    <img src={movie.imagePath} alt='movie poster' />
+        <div>
+            <div className='movie-page'>
+                <div className='movie-page-left'>
+                    <img src={movie.imagePath} />
                 </div>
-                <div className='movie-page-info'>
-                    <h2>{movie.title}</h2>
-                    <p>{movie.description}</p>
+                <div className='movie-page-right'>
+                    <h1>{movie.title}</h1>
+                    {/* <p>{movie.description}</p> */}
                     <p>Rating: {movie.rating}</p>
                 </div>
             </div>
+            <div>
+                <Link to='/'>
+                        <Button variant='primary'>Back to Home</Button>
+                </Link>
+            </div>
+
         </div>
     );
 }
